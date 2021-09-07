b19319b0e913bfe7d8c43f6790f870346a66dc97
Kenneth Long <kdlong@hep.wisc.edu>
2021-09-07 19:26:03 +0200

    Properly show different uncertainties

--- compareDistributions.py
+++ compareDistributions.py
@@ -1,3 +1,4 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import uproot
 import mplhep
@@ -6,6 +7,7 @@ import argparse
 import os
 import logging
 from Utilities import makeSimpleHtml
+logging.basicConfig(level=logging.INFO)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input_files", nargs="*", type=str, help="Input root file(s)", required=True)
@@ -17,6 +19,7 @@ parser.add_argument("-p", "--outputPath", type=str, default=os.path.expanduser("
 parser.add_argument("-o", "--outputFolder", type=str, default="Test")
 parser.add_argument("-a", "--append", type=str, default="", help="string to append to output file name")
 parser.add_argument("-r", "--ratioRange", type=float, nargs=2, default=[0.9, 1.1])
+parser.add_argument("--rawUnc", action='store_true', help="Don't append up or down to uncertainty name")
 parser.add_argument("--noHtml", action='store_true', help="Don't make html in output folder")
 args = parser.parse_args()
 
@@ -24,6 +27,9 @@ if len(args.samples) > 1 and len(args.input_files) != len(args.samples):
     raise RuntimeError("If more than one input file is specified, each file should be specified per sample")
 
 plt.style.use([mplhep.style.ROOT])
+cmap = matplotlib.cm.get_cmap('tab20b')    
+# For a small number of clusters, make them pretty
+all_colors_ = [matplotlib.colors.rgb2hex(cmap(i)) for i in range(cmap.N)]
 
 def plotHists(bins, centralName, datasets, ratioRange=[0.9, 1.1], width=1):
     fig = plt.figure(figsize=(8*width,8))
@@ -31,6 +37,7 @@ def plotHists(bins, centralName, datasets, ratioRange=[0.9, 1.1], width=1):
     ax2 = fig.add_subplot(4, 1, 4) 
     centralHist = datasets[centralName]["hist"]
     for name, dataset in datasets.items():
+        print(name)
         hist = dataset["hist"]
         args = {"label" : name, "color" : dataset["color"]}
         ax1.hist(bins[:-1], bins=bins, weights=hist, histtype='step', **args)
@@ -44,11 +51,19 @@ def plotHists(bins, centralName, datasets, ratioRange=[0.9, 1.1], width=1):
 def compareDistributions():
     samples = args.samples
     filenames = args.input_files 
-    uncertainties = args.uncertainties if args.uncertainties else [""]
-    if len(filenames) != len(samples):
-        filenames = [filenames]*len(args.samples)
-    if len(uncertainties) != len(samples):
-        uncertainties = [uncertainties]*len(args.samples)
+    uncertainties = [""] + args.uncertainties
+    if len(uncertainties) == 2 and len(samples) != 1:
+        logging.info("Will plot uncertainty %s for all variations" % uncertainties[0])
+        uncertainties = uncertainties*len(args.samples)
+    elif len(samples) == 1 and len(uncertainties) > 1:
+        logging.info("Assuming sample %s for all plots" % samples[0])
+        samples = samples*len(uncertainties)
+        print("Length of uncertainties", len(uncertainties))
+    if len(filenames) == 1 and len(samples) > 1:
+        print("Yes, here")
+        filenames = filenames*len(samples)
+        print("len samples", len(samples))
+    print(filenames)
 
     outputPath = "/".join([args.outputPath, args.outputFolder, "plots"])
     if not os.path.isdir(outputPath):
@@ -56,36 +71,48 @@ def compareDistributions():
 
     datasets = {}
     bins = []
-    for sample, filename, unc in zip(samples, filenames, uncertainties):
+    print(samples, filenames, uncertainties)
+
+    for i, (sample, filename, unc) in enumerate(zip(samples, filenames, uncertainties)):
         print(sample, filename, unc)
         rtfile = uproot.open(filename)    
-        hist, bins = rtfile["%s/%s_%s" % (sample, args.hist, args.channel)].to_numpy()
-        cenName = "Central" if unc else sample
-        datasets.update({cenName : 
-                { "hist" : hist,
-                "color" : "red"}
-        })
-        if unc:    
-            histUp,_ = rtfile["%s/%s_%sUp_%s" % (sample, args.hist, unc, args.channel)].to_numpy()
+        cenName = "Central" if unc == "" and len(uncertainties) > 1 else sample
+
+        if unc == "" or args.rawUnc:
+            name = unc
+            histname = "%s/%s_%s_%s" % (sample, args.hist, unc, args.channel)
+            if unc == "":
+                histname = "%s/%s_%s" % (sample, args.hist, args.channel)
+                name = "Central" if len(uncertainties) > 1 else sample
+
+            hist,bins = rtfile[histname].to_numpy()
+            datasets.update({
+                name :
+                    { "hist" : hist,
+                    "color" : all_colors_[i]
+                    },
+            })
+        else:    
+            histUp,bins = rtfile["%s/%s_%sUp_%s" % (sample, args.hist, unc, args.channel)].to_numpy()
             histDown,_ = rtfile["%s/%s_%sDown_%s" % (sample, args.hist, unc, args.channel)].to_numpy()
             datasets.update({
                 unc + " up": 
                     { "hist" : histUp,
-                    "color" : "blue"
+                    "color" : all_colors_[i]
                     },
                 unc + " down": 
                     { "hist" : histDown,
-                    "color" : "blue"
+                    "color" : all_colors_[i]
                     },
             })
     cenName = "Central" if args.uncertainties else samples[0]
     fig = plotHists(bins, cenName, datasets, ratioRange=args.ratioRange)
-    append = args.uncertainties 
+    append = uncertainties[1:min(4, len(uncertainties))]
     if args.append:
         append = append + [args.append]
     outfile = "%s/%s_%s.pdf" % (outputPath, args.hist, "_".join(append))
     fig.savefig(outfile)
-    fig.savefig(outfile.replace("pdf", "png"))
+    fig.savefig(outfile.replace(".pdf", ".png"))
     logging.info(f"Wrote output file {outfile}")
 
     if not args.noHtml:
